76cad4e7afae19de9ab3bd7faae7d1b298ac7610
Wendler <wendler_mendes@hotmail.com>
2020-02-24 12:14:20 -0300

    Estruturando pastas para o projeto

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+//? Conexao com banco de dados
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  user: 'postgres',
+  password: "0814",
+  host: "localhost",
+  port: 5432,
+  database: "launchstore"
+});
